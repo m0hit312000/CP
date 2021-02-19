d5cc233030534ab112f044e85f42480586844e3e
Mohit <mm8085574989@gmail.com>
2021-02-20 01:06:56 +0530

    Increasing Array

--- Introductory Problems/Increasing_Array.cpp
+++ Introductory Problems/Increasing_Array.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 int main()
 {
     int n;
+    long long moves = 0, val;
+    vector<int> a;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> val;
+        a.push_back(val);
+    }
+
+    for (int i = 1; i < n; i++)
+    {
+        while (a[i] < a[i - 1])
+        {
+            a[i]++;
+            moves++;
+        }
+    }
+    cout << moves;
 }
